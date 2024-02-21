46aac266d3822d67f47ca4c6c4d07641f4225d8f
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-02-21 16:26:28 +0100

    Add loader and first commit with computed properties

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,37 +1,46 @@
 <script>
 import axios from 'axios';
 import CharacterItem from './CharacterItem.vue'
+import SpinnerLoading from './SpinnerLoading.vue'
 
 export default {
     name: 'AppMain',
     data() {
         return {
             base_api_url: 'https://rickandmortyapi.com/api/character',
-            characters: []
+            characters: [],
+            loading: true
+        }
+    },
+    computed: {
+        getResults() {
+
+            return this.characters.length > 0 ? "Total results: " + this.characters.length : 'no results yet'
         }
 
     },
     mounted() {
-        console.log(this.base_api_url)
-
         axios
             .get(this.base_api_url)
             .then((response) => {
                 console.log(response.data.results)
                 this.characters = response.data.results;
-                console.log(this.characters)
+                this.loading = false;
 
 
             })
             .catch((error) => {
                 console.log(error.message)
             })
-
     },
     components: {
-        CharacterItem
+        CharacterItem,
+        SpinnerLoading
     }
 }
+/*Use a timeout to verify loader
+setTimeout(()=>{
+}, 3000)*/
 
 </script>
 
@@ -56,12 +65,17 @@ export default {
             </div>
 
 
-
-            <div class="row">
+            <div class="row" v-if="!loading">
                 <CharacterItem v-for="character in characters" :key='character.id + "_character"'
                     :character_item="character">
                 </CharacterItem>
+            </div>
 
+            <SpinnerLoading v-else> </SpinnerLoading>
+
+
+            <div>
+                {{ getResults }}
             </div>
         </div>
 
---
+++ src/components/SpinnerLoading.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'SpinnerLoading'
+}
+</script>
+
+<template>
+    <div class="loader">
+        <i class="fa-solid fa-spinner fa-spin" style="color: #74C0FC;"></i>
+        <div>LOADING...</div>
+    </div>
+</template>
+
+<style scoped>
+.loader {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    >div {
+        padding: 0 1rem;
+    }
+
+}
+</style>
